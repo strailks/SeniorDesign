16f649822f6ce51b7305a8aa60a7f174ce717e46
Cooper Unruh <cunruh2k@gmail.com>
2022-10-27 19:13:07 -0400

    Initial Commit

---
+++ myRoutine/myRoutineApp.swift
@@ -0,0 +1,17 @@
+//
+//  myRoutineApp.swift
+//  myRoutine
+//
+//  Created by Cooper Unruh on 10/27/22.
+//
+
+import SwiftUI
+
+@main
+struct myRoutineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
